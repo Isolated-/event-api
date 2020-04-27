d3d3426b235a5d96c569a4b19ab5dec163da16b5
Michael Palmer <michaelpalmer53@gmail.com>
2020-04-27 19:00:41 +0100

    adds a TransformInterceptor for converting the response after Controller

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map(data => {
+        if (data._doc !== undefined) {
+          delete data._doc._id;
+          delete data._doc.__v;
+        }
+
+        return data;
+      }),
+    );
+  }
+}
