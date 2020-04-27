afeed9a8560bd9e13986dcd169d447de6de39b45
Michael Palmer <michaelpalmer53@gmail.com>
2020-04-27 19:01:07 +0100

    removes unneeded app.e2e-spec.ts and adds Hook e2e test for POST /hook

---
+++ test/hook.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { fakeHook } from '../src/hook/test/util/fakeHook.util';
+import { TransformInterceptor } from '../src/transform.interceptor';
+
+describe('HookController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalInterceptors(new TransformInterceptor());
+
+    await app.init();
+  });
+
+  test('/hook (POST) should return 400 (BadRequest)', () => {
+    return request(app.getHttpServer())
+      .post('/hook')
+      .send(undefined)
+      .expect(400);
+  });
+
+  test('/hook (POST) should return 201 (Created)', () => {
+    const hook = fakeHook();
+
+    return request(app.getHttpServer())
+      .post('/hook')
+      .send(hook)
+      .expect(201)
+      .then(response => {
+        const data = response.body;
+
+        expect(data.hookId).toMatch(
+          /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/,
+        );
+
+        expect(data._id).not.toBeDefined();
+        expect(data.__v).not.toBeDefined();
+
+        delete data.hookId;
+
+        expect(data).toEqual(hook);
+      });
+  });
+
+  test.todo('test 401 and 403 responses from failed authentication');
+
+  afterAll(async () => await app.close());
+});
